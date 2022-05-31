f2265221d949f40151e52a80b89181e07992381c
nando698 <diazfernando07@gmail.com>
2022-05-30 22:30:34 -0300

    desafio 2

--- desafio 2/index.js
+++ desafio 2/index.js
@@ -5,92 +5,108 @@ class Contenedor {
     this.archivo = archivo;
   }
 
-
-/* METODO SAVE */
+  /* METODO SAVE */
 
   async save(objeto) {
-
-// Obtengo ID desde un archivo independiente
-    let id = JSON.parse (await fs.promises.readFile('./ids.txt', 'utf-8'))
-    let maxID = Math.max(...id)
-    objeto.id = maxID + 1
-    id = [...id, objeto.id]
-    await fs.promises.writeFile(`./ids.txt`, JSON.stringify(id))
-
-//Obtengo los productos del archivo
-    let productos = JSON.parse (await fs.promises.readFile('./productos.txt', 'utf-8'))
-
-//Agrego el producto y reescribo el archivo
-    productos.push(objeto)
     
-    await fs.promises.writeFile(`./${this.archivo}`, JSON.stringify(productos))
+    let data = await fs.promises.readFile(`./${this.archivo}`, 'utf-8')
 
-    console.log('Producto agregado con el ID ', objeto.id)
-  }
+        if(!data) {
+            let id = JSON.parse(await fs.promises.readFile("./ids.txt", "utf-8"));
+            let maxID = Math.max(...id);
+            objeto.id = maxID + 1;
+            id = [...id, objeto.id];
+            await fs.promises.writeFile(`./ids.txt`, JSON.stringify(id));
+            
+            await fs.promises.writeFile(`./${this.fileName}`, JSON.stringify(objeto))}
 
+            else {
 
-/* METODO GET BY ID */
-
-async getById(id) {
+    // Obtengo ID desde un archivo independiente
+    let id = JSON.parse(await fs.promises.readFile("./ids.txt", "utf-8"));
+    let maxID = Math.max(...id);
+    objeto.id = maxID + 1;
+    id = [...id, objeto.id];
+    await fs.promises.writeFile(`./ids.txt`, JSON.stringify(id));
 
     //Obtengo los productos del archivo
-    let productos = JSON.parse (await fs.promises.readFile('./productos.txt', 'utf-8'))
+    let productos = JSON.parse(
+      await fs.promises.readFile("./productos.txt", "utf-8")
+    );
 
-    let objeto = productos.find(prod => prod.id == id)
+    //Agrego el producto y reescribo el archivo
+    productos.push(objeto);
 
-    console.log(objeto ? objeto : 'ese ID no existe')
+    await fs.promises.writeFile(`./${this.archivo}`, JSON.stringify(productos));
 
-}
+    console.log("Producto agregado con el ID ", objeto.id);
+  }}
 
-/* METODO GETALL */
+  /* METODO GET BY ID */
 
-async getAll() {
-    let productos = JSON.parse (await fs.promises.readFile('./productos.txt', 'utf-8'))
-    console.log(productos)
-}
+  async getById(id) {
+    //Obtengo los productos del archivo
+    let productos = JSON.parse(
+      await fs.promises.readFile("./productos.txt", "utf-8")
+    );
 
+    let objeto = productos.find((prod) => prod.id == id);
 
-/* METODO DELETE BY ID */
+    console.log(objeto ? objeto : "ese ID no existe");
+  }
 
-async deleteById(id){
+  /* METODO GETALL */
 
-    let productos = JSON.parse (await fs.promises.readFile('./productos.txt', 'utf-8'))
+  async getAll() {
+    let productos = JSON.parse(
+      await fs.promises.readFile("./productos.txt", "utf-8")
+    );
+    console.log(productos);
+  }
+
+  /* METODO DELETE BY ID */
+
+  async deleteById(id) {
+    let productos = JSON.parse(
+      await fs.promises.readFile("./productos.txt", "utf-8")
+    );
+
+    if (productos.some((prod) => prod.id == id)) {
+      let newProductos = productos.filter((prod) => prod.id != id);
+
+      await fs.promises.writeFile(
+        `./${this.archivo}`,
+        JSON.stringify(newProductos)
+      );
+      console.log("producto eliminado");
+    } else {
+      console.log("no existe producto con ese id");
+    }
+  }
+
+  /* METODO DELETE ALL */
+
+  async deleteAll() {
     
-    if (productos.some( prod=> prod.id == id)){
+    try {
     
-    let newProductos = productos.filter(prod => prod.id != id)
+    await fs.promises.writeFile(`./${this.archivo}`, "[]");
 
-    await fs.promises.writeFile(`./${this.archivo}`, JSON.stringify(newProductos))
-console.log('producto eliminado')
-}else {
-    console.log('no existe producto con ese id')
-}}
-
-
-/* METODO DELETE ALL */
-
-async deleteAll(){
-    await fs.promises.writeFile(`./${this.archivo}`, '[]')
-
-    console.log('Todos los archivos han sido eliminados')
+    console.log("Todos los archivos han sido eliminados");
+    }catch(error) {
+        console.log(error)
+    }
+  }
 }
-
-
-}
-
 
 /* TESTEO DE METODOS */
 
-
 //new Contenedor('productos.txt').save({nombre: 'shampoo', precio: 200, thumbnail: 'ejemplo url'})
 
-//new Contenedor('productos.txt').getById(1)
+//new Contenedor('productos.txt').getById(8)
 
 //new Contenedor('productos.txt').getAll()
 
 //new Contenedor('productos.txt').deleteById(6)
 
 //new Contenedor('productos.txt').deleteAll()
-
-
-
