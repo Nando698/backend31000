c7857ac6983b08625816efd70cb70a0703c24eb8
Nando698 <diazfernando07@gmail.com>
2022-06-05 20:40:56 -0300

    desafio 3

--- desafio3/index.js
+++ desafio3/index.js
@@ -1,8 +1,28 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = 8080
-let contador = 0
 let fecha = new Date()
+
+class Contenedor {
+    constructor(archivo) {
+      this.archivo = archivo;
+    }
+
+    async getAll() {
+        try {
+          return JSON.parse(
+            await fs.promises.readFile(`./desafio3/${this.archivo}`, "utf-8")
+          );
+        } catch (error) {
+          console.log("[[[ error desde metodo getAll ]]]", error);
+        }
+      }
+
+
+}
+
+
 
 app.listen(port, () => {
     try {
@@ -16,32 +36,39 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send(
 
-       ` <h1 style="color: blue" >Bienvenido al servidor de NANDO</h1> `
+       ` <h1 style="color: blue" >Bienvenido al servidor de NNANDO</h1> `
 
     )
 } )
 
 
-app.get('/visitas', (req, res) => {
+app.get('/productos', async (req, res) => {
     
-    contador += 1
+    let productos = await new Contenedor('productos.txt').getAll()
     
+    res.send( `<h1>PRODUCTOS</h1> <ul  style="list-style: none" > ${productos.map(prod => {
+
+        let card = `<li><img src='${prod.thumbnail}' style="width: 30px" />${prod.title}, <br> Precio:$ ${prod.price}</li>`
+       
+       
+        return card
+
+
+    })}</ul>` )
+} )
+
+
+app.get('/productoRandom', async (req, res) => {
+    
+    let productos = await new Contenedor('productos.txt').getAll()
+
+    let random =Math.floor( Math.random() * productos.length);
     
     res.send(
 
-       ` visitante nro ${contador}`
+        `<img src='${productos[random].thumbnail}' style="width: 100px" /><br>${productos[random].title}, <br> Precio:$ ${productos[random].price}`
 
     )
 } )
 
 
-app.get('/fyh', (req, res) => {
-    
-    
-    
-    
-    res.json(
-
-       fecha
-    )
-} )
